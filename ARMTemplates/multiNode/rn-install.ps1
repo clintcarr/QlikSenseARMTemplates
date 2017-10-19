14843dc2965af9c60dd53064d3ed52c19844a6a5
clint <clint.carr@outlook.com>
2017-10-20 10:38:35 +1100

    Small change to load balance script

--- ARMTemplates/multiNode/rn-install.ps1
+++ ARMTemplates/multiNode/rn-install.ps1
@@ -125,11 +125,9 @@ Write-Log -Message "Connecting to Qlik Sense on $qlikSenseCentralNode"
 $hostNameRim = $env:COMPUTERNAME
 c:\programdata\chocolatey\bin\psexec.exe -h -u $env:COMPUTERNAME\$adminUser -p $adminPass /accepteula cmd /c "powershell -command Connect-Qlik $($qlikSenseCentralNode) -TrustAllCerts -UseDefaultCredentials; register-qliknode -hostname $hostNameRim -name Rim -nodePurpose 2 -engineEnabled -proxyEnabled" >c:\installation\registerQlik.txt 2>&1
 
-# Now server is added certificates have been installed, connection to Qlik Sense can occur without elevation
-Connect-Qlik $qlikSenseCentralNode -UseDefaultCredentials
-
 Write-Log -Message "Updating Virtual Proxies on both Central and Rim node to support load balancing of both nodes."
-Update-QlikVirtualProxy -id $(Get-QlikVirtualProxy -filter "description eq 'Rim Proxy (Default)'").id -loadBalancingServerNodes $(Get-QlikNode -filter "name eq 'Rim'").id, $(Get-QlikNode -filter "name eq 'Central'").id
-Update-QlikVirtualProxy -id $(Get-QlikVirtualProxy -filter "description eq 'Central Proxy (Default)'").id -loadBalancingServerNodes $(Get-QlikNode -filter "name eq 'Rim'").id, $(Get-QlikNode -filter "name eq 'Central'").id
+c:\programdata\chocolatey\bin\psexec.exe -h -u $env:COMPUTERNAME\$adminUser -p $adminPass /accepteula cmd /c "powershell -command Connect-Qlik $($qlikSenseCentralNode) -TrustAllCerts -UseDefaultCredentials; Update-QlikVirtualProxy -id $(Get-QlikVirtualProxy -filter "description eq 'Rim Proxy (Default)'").id -loadBalancingServerNodes $(Get-QlikNode -filter "name eq 'Rim'").id, $(Get-QlikNode -filter "name eq 'Central'").id;Update-QlikVirtualProxy -id $(Get-QlikVirtualProxy -filter "description eq 'Central Proxy (Default)'").id -loadBalancingServerNodes $(Get-QlikNode -filter "name eq 'Rim'").id, $(Get-QlikNode -filter "name eq 'Central'").id" >c:\installation\vPLoadBalance.txt 2>&1
+
+
 
 Write-Log "Server provisioning complete"
